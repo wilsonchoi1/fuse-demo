81f509d01bf8cca2e09ed0e5a9515d1b109a7e74
wilsonchoi1 <eewai97@hotmail.com>
2018-01-11 16:53:51 +0800

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE customerdemo (
-	customerID varchar(10) NOT NULL,
-	vipStatus varchar(10) NOT NULL ,  
-	balance integer NOT NULL
-);
+#CREATE TABLE customerdemo (
+#	customerID varchar(10) NOT NULL,
+#	vipStatus varchar(10) NOT NULL ,  
+#	balance integer NOT NULL
+#);
 
-INSERT INTO customerdemo (customerID,vipStatus,balance) VALUES ('A01','Diamond',1000);
-INSERT INTO customerdemo (customerID,vipStatus,balance) VALUES ('A02','Gold',500);
+#INSERT INTO customerdemo (customerID,vipStatus,balance) VALUES ('A01','Diamond',1000);
+#INSERT INTO customerdemo (customerID,vipStatus,balance) VALUES ('A02','Gold',500);
